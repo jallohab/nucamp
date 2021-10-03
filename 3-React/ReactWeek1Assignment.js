82579544b4e33176b34b195abdd0601da41c2125
Abdulai <jalloha92@gmail.com>
2021-10-03 03:49:43 -0400

    First folder or something... I don't know

---
+++ 3-React/ReactWeek1Assignment.js
@@ -0,0 +1,32 @@
+class Student {
+    constructor(name, email, community) {
+        this.name = name;
+        this.email = email;
+        this.community = community;
+    }
+}
+
+class Bootcamp {
+    constructor(name, level, students=[]) {
+        this.name = name;
+        this.level = level;
+        this.students = students;
+    }
+    registerStudent(bjStu) {
+            if(this.students.filter(s => s.email === bjStu.email).length) {
+                console.log(`Student is registered`);
+            } else {
+                this.students.push(bjStu);
+                console.log(`Registering ${bjStu.email} to the bootcamp ${this.name}.`);
+            }
+        }
+        
+    
+}
+
+const a = new Student( "Abdulai", "abdu@gmail.com", "Atlanta");
+const b = new Student( "Jalloh", "abdu@gmail.com", "New York");
+
+const bootcampp = new Bootcamp("Abdu", "okay");
+bootcampp.registerStudent(a);
+bootcampp.registerStudent(b);
