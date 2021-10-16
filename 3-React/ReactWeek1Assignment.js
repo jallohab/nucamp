7d2e6bdda346e05d28c71e2d8eb309b9511b9265
Abdulai <jalloha92@gmail.com>
2021-10-16 02:42:40 -0400

    Header/Footer late night learning

--- 3-React/ReactWeek1Assignment.js
+++ 3-React/ReactWeek1Assignment.js
@@ -1,31 +1,29 @@
 class Student {
-    constructor(name, email, community) {
-        this.name = name;
-        this.email = email;
-        this.community = community;
-    }
+  constructor(name, email, community) {
+    this.name = name;
+    this.email = email;
+    this.community = community;
+  }
 }
 
 class Bootcamp {
-    constructor(name, level, students=[]) {
-        this.name = name;
-        this.level = level;
-        this.students = students;
+  constructor(name, level, students = []) {
+    this.name = name;
+    this.level = level;
+    this.students = students;
+  }
+  registerStudent(bjStu) {
+    if (this.students.filter((s) => s.email === bjStu.email).length) {
+      console.log(`Student is registered`);
+    } else {
+      this.students.push(bjStu);
+      console.log(`Registering ${bjStu.email} to the bootcamp ${this.name}.`);
     }
-    registerStudent(bjStu) {
-            if(this.students.filter(s => s.email === bjStu.email).length) {
-                console.log(`Student is registered`);
-            } else {
-                this.students.push(bjStu);
-                console.log(`Registering ${bjStu.email} to the bootcamp ${this.name}.`);
-            }
-        }
-        
-    
+  }
 }
 
-const a = new Student( "Abdulai", "abdu@gmail.com", "Atlanta");
-const b = new Student( "Jalloh", "abdu@gmail.com", "New York");
+const a = new Student("Abdulai", "abdu@gmail.com", "Atlanta");
+const b = new Student("Jalloh", "abdu@gmail.com", "New York");
 
 const bootcampp = new Bootcamp("Abdu", "okay");
 bootcampp.registerStudent(a);
